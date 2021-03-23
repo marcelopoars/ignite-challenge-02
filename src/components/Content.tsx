8c9c520ef6954743d8e9e5aa6c194235394c2af1
marcelo-pereira-axur <marcelo.pereira@axur.com>
2021-03-23 08:51:26 -0300

    Finalizado desafio.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+import "../styles/content.scss";
+
+interface ContentProps {
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Runtime: string;
+    Ratings: Array<{ Value: string }>;
+    imdbID?: string;
+  }>;
+}
+
+export function Content({ movies }: ContentProps) {
   // Complete aqui
+
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import "../styles/footer.scss";
+
+export function Footer() {
+  return (
+    <footer>
+      <p>
+        Ignite - Desafio 02 | Desenvolvido com ❤ por{" "}
+        <a href="https://marcelopereira.dev" target="_blank" rel="noreferrer">
+          Marcelo Pereira
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
